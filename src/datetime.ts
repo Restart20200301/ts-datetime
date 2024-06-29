ae341105c3dd76bca16ae762b5416608aacf8368
hugohsu <hugohsu@tencent.com>
2024-06-29 16:54:48 +0800

    start

--- src/datetime.ts
+++ src/datetime.ts
@@ -1,59 +1,42 @@
-const secondsPerMinute = 60
-const minutesPerHour = 60
-const hoursPerDay = 24
-
-const millisPerSecond = 1000
-const millisPerMinute = millisPerSecond * secondsPerMinute
-const millisPerHour = millisPerMinute * minutesPerHour
-const millisPerDay = millisPerHour * hoursPerDay
-
 const ticksPerMillisecond = 1
-const ticksPerSecond = ticksPerMillisecond * millisPerSecond
-const ticksPerMinute = ticksPerSecond * secondsPerMinute
-const ticksPerHour = ticksPerMinute * minutesPerHour
-const ticksPerDay = ticksPerHour * hoursPerDay
+const ticksPerSecond = ticksPerMillisecond * 1000
+const ticksPerMinute = ticksPerSecond * 60
+const ticksPerHour = ticksPerMinute * 60
+const ticksPerDay = ticksPerHour * 24
 
-class DateTime {
-  private readonly data: number
+const daysPerYear = 365
+const daysPer4Years = daysPerYear * 4 + 1
+const daysPer100Years = daysPer4Years * 25 - 1
+const daysPer400Years = daysPer100Years * 4 + 1
 
-  constructor(ticks: number) {
-    this.data = ticks
-  }
-
-  addTicks(value: number) {
-    return this.add(value, 1)
-  }
+class Datetime {
+  constructor(private readonly ticks: number) {}
 
   addMilliseconds(value: number) {
-    return this.add(value, ticksPerMillisecond)
+    return this.add(value * ticksPerMillisecond)
   }
 
   addSeconds(value: number) {
-    return this.add(value, ticksPerSecond)
+    return this.add(value * ticksPerSecond)
   }
 
   addMinutes(value: number) {
-    return this.add(value, ticksPerMinute)
+    return this.add(value * ticksPerMinute)
   }
 
   addHours(value: number) {
-    return this.add(value, ticksPerHour)
+    return this.add(value * ticksPerHour)
   }
 
   addDays(value: number) {
-    return this.add(value, ticksPerDay)
+    return this.add(value * ticksPerDay)
   }
 
-  addMonths(value: number) {
-    const [y, m, d] = this.getYMD()
-  }
+  addMonths(value: number) {}
 
-  private add(value: number, scale: number): DateTime {
-    return new DateTime(value * scale)
-  }
+  addYears(value: number) {}
 
-  private getYMD(): [number, number, number] {
-    const date = new Date(this.data / ticksPerMillisecond)
-    return [date.getFullYear(), date.getMonth(), date.getDate()]
+  private add(ticks: number) {
+    return new Datetime(ticks)
   }
 }
